2a092bbce267bb845519378cfa149fe6c0091f5e
Cam <166952994+zCam12@users.noreply.github.com>
2024-04-13 23:14:02 -0400

    Update avatar.js

--- commands/utility/avatar.js
+++ commands/utility/avatar.js
@@ -6,23 +6,23 @@ module.exports = {
 		.setDescription('Displays avatar of mnetioned user or your own')
         .addMentionableOption(option => option.setName('person').setDescription('The person\'s whos avatar you\'d like.')),
 	async execute(interaction) {
-
+	
 		var target = interaction.options.getMentionable('person') || interaction;
 		target = target.user;
-
+	
 		// Find color role
-        var color = null;
-        var colorRole = null;
-        const userRoles = interaction.member._roles;
-        const rolesLength = interaction.member._roles.length;
-
-        for(var i = 0; i <= rolesLength - 1; i++){
-            if(interaction.guild.roles.cache.get(userRoles[i]).hoist == true){
-                colorRole = userRoles[i];
-                color = interaction.guild.roles.cache.get(userRoles[i]).hexColor;
-            }
-        };
-
+	        var color = null;
+	        var colorRole = null;
+	        const userRoles = interaction.member._roles;
+	        const rolesLength = interaction.member._roles.length;
+	
+	        for(var i = 0; i <= rolesLength - 1; i++){
+	            if(interaction.guild.roles.cache.get(userRoles[i]).hoist == true){
+	                colorRole = userRoles[i];
+	                color = interaction.guild.roles.cache.get(userRoles[i]).hexColor;
+	            }
+	        };
+	
 		// Create embed
 		const embed = new EmbedBuilder()
 			.setColor(color)
