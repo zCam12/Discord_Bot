6525455cce411b17622670b1f02bdd62f64bf687
Cam <166952994+zCam12@users.noreply.github.com>
2024-04-13 20:22:59 -0400

    Add files via upload

---
+++ commands/utility/avatar.js
@@ -0,0 +1,39 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('avatar')
+		.setDescription('Displays avatar of mnetioned user or your own')
+        .addMentionableOption(option => option.setName('person').setDescription('The person\'s whos avatar you\'d like.')),
+	async execute(interaction) {
+		var target;
+
+		if(interaction.options.getMentionable('person') == null){
+			target = interaction.user;
+		}else{
+			target = interaction.options.getMentionable('person');
+		}
+
+		// Find color role
+        var color = null;
+        var colorRole = null;
+        const userRoles = interaction.member._roles;
+        const rolesLength = interaction.member._roles.length;
+
+        for(var i = 0; i <= rolesLength - 1; i++){
+            if(interaction.guild.roles.cache.get(userRoles[i]).hoist == true){
+                colorRole = userRoles[i];
+                color = interaction.guild.roles.cache.get(userRoles[i]).hexColor;
+            }
+        };
+
+		// Create embed
+		const embed = new EmbedBuilder()
+			.setColor(color)
+			.setImage(target.displayAvatarURL({ dynamic: true }))
+			.setFooter(footer = { text: (`Requested by ${interaction.user.username}`) });
+
+
+		return interaction.reply({ embeds: [embed] });
+	},
+};
